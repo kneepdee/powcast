fe339e66cf313e4785f2c8534648c9d1c81e8b73
kneepdee <gabriel.kuslap@gmail.com>
2018-12-08 17:45:06 +0200

    Add get current location functionality

--- scripts.js
+++ scripts.js
@@ -1,60 +1,84 @@
 const message = document.querySelector('.message');
 const locationInput = document.querySelector('.location-input');
+const myLocationBtn = document.querySelector('.my-location-btn');
+
+const apiKey = 'ae498ec7d2c12a488f23a06e510990ad'
+const lines = 8;
 
 // check if key === enter
 const enterPressed = event => {
   if (event.key === 'Enter') {
-    displayResult();
+    displayResultInput();
   }
 }
 
-// listen for key pressed while in search box
-locationInput.addEventListener('keyup', enterPressed);
 
-const displayResult = () => {
-  const apiKey = 'ae498ec7d2c12a488f23a06e510990ad'
+const getMyLocation = () => {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const latLon = [position.coords.latitude, position.coords.longitude];
+    return latLon;
+    // console.log(latLon);
+  });
+}
+
+const loc = getMyLocation();
+console.log(loc);
+
+// let lat, lon;
+// const loc = getMyLocation();
+// console.log(loc);
+// [lat, lon] = getMyLocation();
+// console.log(lat, lon);
+
+
+
+const displayResultMyLocation = () => {
+  if (!navigator.geolocation) {
+    message.innerHTML = 'Geolocation is not supported by your browser';
+    return;
+  }
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=${apiKey}&cnt=${lines}`;
+  fetchData(url);
+}
+
+
+const displayResultInput = () => {
   const place = locationInput.value;
   // number of 3h forecasts. 8 * 3 = 24h
-  const lines = 8;
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${place}&APPID=${apiKey}&cnt=${lines}`;
   console.log(place);
+  fetchData(url);
+}
+
+const fetchData = url => {
   fetch(url).then(function (result) {
     return result.json();
   }).then(function (json) {
     console.log(json);
-    let total = 0;
+    let totalSnow = 0;
+    if (json.message === 'city not found') {
+      console.log('not found');
+      message.innerHTML = 'Location not found';
+      return;
+    }
     json.list.forEach(element => {
       if (element.hasOwnProperty('snow')) {
         const volumeThreeHours = element.snow['3h'];
         if (volumeThreeHours !== undefined) {
-          total += volumeThreeHours;
+          totalSnow += volumeThreeHours;
         }
       } else {
         console.log('no snow');
       }
     });
-    console.log(total);
-    message.innerHTML = `${json.city.name}, ${json.city.country}, ${total}`;
-  });
+    console.log(totalSnow);
+    message.innerHTML = `${json.city.name}, ${json.city.country}, ${totalSnow}`;
+  })
 }
 
-
-// fetch the json weather data
-
-// fetch(url).then(function (result) {
-//   return result.json();
-// }).then(function (json) {
-//   console.log(json);
-//   let total = 0;
-//   // add any function you wanna run here
-//   json.list.forEach(element => {
-//     const volumeThreeHours = element.snow['3h'];
-//     if (volumeThreeHours !== undefined) {
-//       total += volumeThreeHours;
-//     }
-//   });
-//   console.log(total);
-// });
+// listen for key pressed while in search box
+locationInput.addEventListener('keyup', enterPressed);
+myLocationBtn.addEventListener('click', displayResultMyLocation);
 
 
 // INFOS
@@ -63,14 +87,11 @@ const displayResult = () => {
 
 // TODO
 
-// get snowfall amount for next 24h
-
-// user can choose his location and get forecast
 // or user can choose any location and get forecast
-
-// shows amount of snow in cm
 
 // if snow > 10cm -> POWDAY
 // else 
 // show amount of snow
 // or show NO SNOW
+
+// add error message when location not found
